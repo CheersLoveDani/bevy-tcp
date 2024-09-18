ac05b15ccde4683a68d06a8c29470fe8b5f11514
Dani Fullerton <kingme441@gmail.com>
2024-09-18 01:42:40 +0100

    bevy is confusing

--- src/main.rs
+++ src/main.rs
@@ -11,31 +11,29 @@ pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
         app.add_systems(Startup, add_people);
-        app.add_systems(Update, (hello_world, (update_people, greet_people).chain()));
+        app.add_systems(Update, (update_people, greet_people).chain());
     }
 }
 
-// Startup system
 fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Elaina Proctor".to_string())));
     commands.spawn((Person, Name("Renzo Hume".to_string())));
     commands.spawn((Person, Name("Zayna Nieves".to_string())));
 }
 
-//Update system
-fn hello_world() {
-    println!("hello world!");
-}
-
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    // update our timer with the time elapsed since the last update
+    // if that caused the timer to finish, we say hello to everyone
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
     }
 }
 
 fn update_people(mut query: Query<&mut Name, With<Person>>) {
-    println!("Updating people...");
     for mut name in &mut query {
         if name.0 == "Elaina Proctor" {
             name.0 = "Elaina Hume".to_string();
@@ -49,3 +47,6 @@ struct Person;
 
 #[derive(Component)]
 struct Name(String);
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
